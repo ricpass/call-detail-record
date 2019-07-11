e9473fdc81ea60826a6f7f8bdf56361dca059859
ricpass <ricpass@users.noreply.github.com>
2019-07-11 21:27:14 +0100

    added project skeleton

---
+++ src/main/java/com/ricardopassarella/CallDetailRecordApplication.java
@@ -0,0 +1,13 @@
+package com.ricardopassarella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallDetailRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallDetailRecordApplication.class, args);
+    }
+
+}
